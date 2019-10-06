d72b40202285cb6cdc36d0b216e2c457852c0e34
jayak1991 <jayakrishnanalli.ror@gmail.com>
2019-10-06 22:23:31 +0530

    Implemented the ability to delete records

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,34 +1,51 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show]
-  def index
-    @posts = Post.all
-  end
+	before_action :set_post, only: [:show, :edit, :update, :destroy]
 
-  def new
-    @post = Post.new
-  end
+	def index
+		@posts = Post.all
+	end
 
-  def create
-    @post = Post.new(post_params)
-    @post.user_id = current_user.id
-    if @post.save
-      redirect_to @post, notice: 'Your post was created successfully'
-    else
-      render 'new'
-    end
-  end
+	def new
+		@post = Post.new
+	end
 
-  def show
-  end
+	def create
+		@post = Post.new(post_params)
+		@post.user_id = current_user.id
 
-  private
+		if @post.save
+			redirect_to @post, notice: 'Your post was created successfully'
+		else
+			render :new
+		end
+	end
 
-  def post_params
-    params.require(:post).permit(:date, :rationale)
-  end
+	def edit
+	end
 
-  def set_post
-    @post = Post.find(params[:id])
-  end
+	def update
+		if @post.update(post_params)
+			redirect_to @post, notice: 'Your post was edited successfully'
+		else
+			render :edit
+		end
+	end
 
+	def destroy
+		@post.delete
+		redirect_to posts_path, notice: 'Your post was deleted successfully'
+	end
+
+	def show
+	end
+
+	private
+
+	  def post_params
+	  	params.require(:post).permit(:date, :rationale)
+	  end
+
+	  def set_post
+	  	@post = Post.find(params[:id])
+	  end
 end
