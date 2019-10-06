46e2e834e5ef289fc39d127ddb376d62361a9dc2
jayak1991 <jayakrishnanalli.ror@gmail.com>
2019-10-06 17:58:27 +0530

    Refactored index page to use Post partail

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,9 @@
+@user = User.create(email: "test@test.com", password: "asdafsf", password_confirmation: "asdafsf", first_name: "Jon", last_name: "Snow")
+
+puts "1 User created"
+
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content")
+	Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
 
 puts "100 Posts have been created"
